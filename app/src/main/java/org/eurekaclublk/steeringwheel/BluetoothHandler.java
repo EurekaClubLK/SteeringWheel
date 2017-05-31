c8b61bbea7b6143a0bfbe8bbce4c4bb477f0d5b1
cie6868 <cie6868@yahoo.co.in>
2017-05-31 15:15:05 +0530

    Added javadoc comments.

--- app/src/main/java/org/eurekaclublk/steeringwheel/BluetoothHandler.java
+++ app/src/main/java/org/eurekaclublk/steeringwheel/BluetoothHandler.java
@@ -13,7 +13,20 @@ import java.io.PrintWriter;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Interfaces with the BluetoothAdapter class to create a SPP byte stream.
+ * Connects, disconnects, sends and receives strings.
+ * Uses default UUID for the HC-06 receiver.
+ *
+ * @author  Chamath Ellawala
+ * @since   29/05/2017
+ *
+ */
 class BluetoothHandler {
+
+    // TODO authentication
+    // TODO fixed format
+    // TODO input stream
 
     private static final UUID HC06_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final BluetoothAdapter BLUETOOTH_ADAPTER = BluetoothAdapter.getDefaultAdapter();
@@ -22,6 +35,11 @@ class BluetoothHandler {
     private BufferedInputStream _socketIn;
     private PrintWriter _socketOut;
 
+    /**
+     * Teleports user to Bluetooth settings page if Bluetooth is not enabled.
+     *
+     * @param activity Calling activity
+     */
     static void enableBluetooth(Activity activity) {
         if (!isBluetoothEnabled()) {
             Intent bIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -29,22 +47,44 @@ class BluetoothHandler {
         }
     }
 
+    /**
+     * Checks if Bluetooth is enabled.
+     *
+     * @return Bluetooth is enabled
+     */
     static boolean isBluetoothEnabled() {
         return BLUETOOTH_ADAPTER.isEnabled();
     }
 
+    /**
+     * Gets a list of paired devices from BluetoothAdapter.
+     *
+     * @return Array of BluetoothDevice
+     */
     static BluetoothDevice[] getPairedDevices() {
         Set<BluetoothDevice> devices = BLUETOOTH_ADAPTER.getBondedDevices();
         return devices.toArray(new BluetoothDevice[devices.size()]);
     }
 
+    /**
+     * Generates a new instance of BluetoothHandler with an active connection.
+     *
+     * @param device BluetoothDevice object corresponding to the desired paired device
+     * @return Instance of self with active connection
+     * @throws IOException On connection failure
+     */
     static BluetoothHandler newConnection(BluetoothDevice device) throws IOException {
         BluetoothHandler bh = new BluetoothHandler();
         bh.connect(device);
         return bh;
     }
 
-    void connect(BluetoothDevice device) throws IOException {
+    /**
+     * Connects to the specified device.
+     *
+     * @param device Instance of BluetoothDevice corresponding to the desired paired device
+     */
+    private void connect(BluetoothDevice device) throws IOException {
         _socket = device.createRfcommSocketToServiceRecord(HC06_UUID);
         _socket.connect();
 
@@ -52,10 +92,20 @@ class BluetoothHandler {
         _socketOut = new PrintWriter(new BufferedOutputStream(_socket.getOutputStream()));
     }
 
+    /**
+     * Checks if this instance of BluetoothHandler already has an active connection.
+     *
+     * @return Connected to a device
+     */
     boolean isConnected() {
         return _socket.isConnected();
     }
 
+    /**
+     * Disconnects from the active device. This instance of BluetoothHandler should not be reused.
+     *
+     * @throws IOException On failure to disconnect
+     */
     void disconnect() throws IOException {
         if (isConnected()) {
             _socketIn.close();
@@ -64,6 +114,12 @@ class BluetoothHandler {
         }
     }
 
+    /**
+     * Send a string to the active device.
+     *
+     * @param text String to be sent to the device
+     * @throws IOException On failure to send
+     */
     void write(String text) throws IOException {
         if (isConnected()) {
             _socketOut.write(text);
--- app/src/main/java/org/eurekaclublk/steeringwheel/MainActivity.java
+++ app/src/main/java/org/eurekaclublk/steeringwheel/MainActivity.java
@@ -16,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothHandler bluetoothHandler;
 
+    /**
+     * When the activity is first created (opened app from scratch).
+     *
+     * @param savedInstanceState Any state to be restored for the appearance of continuity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // temporary button to send a string
         Button btnTest = (Button)findViewById(R.id.activity_main_btnSend);
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,43 +47,72 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * When the Activity is displayed (opened app from scratch or switched back from another app).
+     * Redirects to Bluetooth settings page if Bluetooth is not enabled.
+     * Displays dialog to select the paired device.
+     */
     @Override
     protected void onStart() {
         super.onStart();
 
-        BluetoothHandler.enableBluetooth(this);
+        // redirect user to Bluetooth settings if functionality is off
+        if (!BluetoothHandler.isBluetoothEnabled())
+            BluetoothHandler.enableBluetooth(this);
 
+        // show device selection dialog
         BluetoothDevice[] devices = BluetoothHandler.getPairedDevices();
         showBluetoothSelectionDialog(devices);
     }
 
+    /**
+     * When the Activity is removed from view (closed app or switched to another app).
+     * Forces disconnect from device.
+     */
     @Override
     protected void onPause() {
         super.onPause();
 
+        // disconnect from device before closing
         if (bluetoothHandler != null)
             if (bluetoothHandler.isConnected())
                 disconnectFromDevice();
     }
 
+    /**
+     * Displays an AlertDialog with a list of paired device names.
+     * Connects to the selected device.
+     *
+     * @param devices Array of BluetoothDevice objects for paired devices
+     */
     private void showBluetoothSelectionDialog(final BluetoothDevice[] devices) {
+        // generate an array of device names to be displayed
         String[] deviceNames = new String[devices.length];
         for (int i = 0; i < devices.length; i++)
             deviceNames[i] = devices[i].getName();
 
+        // display the list
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
         alertBuilder.setTitle(getString(R.string.activity_main_dialog_title));
         alertBuilder.setItems(deviceNames, new DialogInterface.OnClickListener() {
+            // on user selection
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // close the dialog
                 dialog.dismiss();
 
+                // try to connect
                 connectToDevice(devices[which]);
             }
         });
         alertBuilder.show();
     }
 
+    /**
+     * Connects to the specified device. Displays and logs error on failure. Displays Snackbar on success.
+     *
+     * @param device BluetoothDevice object corresponding to the desired paired device
+     */
     private void connectToDevice(BluetoothDevice device) {
         try {
             bluetoothHandler = BluetoothHandler.newConnection(device);
@@ -88,6 +123,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Disconnects from connected device. Displays and logs error on failure.
+     */
     private void disconnectFromDevice() {
         try {
             bluetoothHandler.disconnect();
@@ -97,12 +135,23 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Shows a Snackbar for the desired string.
+     *
+     * @param message Identifier of the desired string
+     * @param args Any arguments to be injected into the message string
+     */
     private void showSnackbar(int message, Object... args) {
         String messageString = getString(message, args);
         View coordinatorLayout = findViewById(R.id.activity_main_coordinatorlayout);
         Snackbar.make(coordinatorLayout, messageString, Snackbar.LENGTH_SHORT).show();
     }
 
+    /**
+     * Shows an AlertDialog for the specific error string.
+     *
+     * @param errorMessage Identifier of the desired error string
+     */
     private void showErrorDialog(int errorMessage) {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
         alertBuilder.setTitle(R.string.activity_main_error_title);
